43e9d19a0bca0e6446b8db73e115d78dc57f98f6
X-y-l <georgetownsend@yahoo.com>
2023-02-26 17:55:49 +0000

    fingles

--- angles_testing.py
+++ angles_testing.py
@@ -8,8 +8,20 @@ def angle_between(v1, v2):
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
-def finger_angles(vects):
+def finger_angle(vects):
     total_curl = 0
     for i in range(len(vects)-1):
         total_curl += angle_between(vects[i], vects[i+1])
     return total_curl
+
+def point_to_cor(point):
+    cor = [point.x, point.y, point.z]
+    return cor
+
+def hand_curl_vals(hand):
+    fingers = []
+    for i in range(1,21,4):
+        current_finger = [point_to_cor(hand[0])]
+        current_finger += [point_to_cor(hand[j]) for j in range(i,i+5)]
+        fingers.append(finger_angle(current_finger))
+    return fingers
