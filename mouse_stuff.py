f103bce6b10307754fbb2857a8754711567551ff
X-y-l <georgetownsend@yahoo.com>
2023-02-26 17:17:27 +0000

    mouse movement stuff

--- mouse_stuff.py
+++ mouse_stuff.py
@@ -1,8 +1,13 @@
 import mouse
-import time
+import numpy as np
+import pyautogui as pagui
 
-for i in range(10):
-    mouse.click('left')
-    time.sleep(1)
+width, height = pagui.size()
 
+def move_mouse(old_pos_hand, new_pos_hand):
 
+    old_pos_mouse = [old_pos_hand.x * width, old_pos_hand.y*height]
+    new_pos_mouse = [new_pos_hand.x * width, new_pos_hand.y*height]
+
+    move_vect = [new_pos_mouse[0] - old_pos_mouse[0], new_pos_mouse[1] - old_pos_mouse[1]]
+    mouse.move(move_vect[0], -move_vect[1], False)
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # Readme
-Does stuff
+## What is this?
+This is a program made for BrisHack 2023 that uses a camera instead of a keyboard and mouse for inputs. The plan is to have hand tracking to control the mouse movements, and to use speech recognition as keyboard inputs.
